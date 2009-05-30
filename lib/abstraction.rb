b1b9d3eebde84b832cdb4f3a7d52abc1ab085d46
Peter Jaros <peter.a.jaros@gmail.com>
2009-05-30 12:02:05 -0400

    Refactor to work on 1.8.6.

--- lib/abstraction.rb
+++ lib/abstraction.rb
@@ -1,36 +1,26 @@
 class AbstractClassError < StandardError; end
 
-# metaid
-class Object
-  # The hidden singleton lurks behind everyone
-  def metaclass; class << self; self; end; end
-  def meta_eval &blk; metaclass.instance_eval &blk; end
-  
-  # Adds methods to a metaclass
-  def meta_def name, &blk
-    meta_eval { define_method name, &blk }
-  end
-  
-  # Defines an instance method within a class
-  def class_def name, &blk
-    class_eval { define_method name, &blk }
-  end
-end
-
 class Class
   def abstract
-    abstract_class = self
+    @abstract_class = true
     
-    raise_if_abstract = lambda do |*args, &block|
-      if self == abstract_class
-        raise AbstractClassError, "#{self} is an abstract class and cannot be instantiated"
-      else
-        super *args, &block
+    self.extend Module.new {
+      def new(*args, &block)
+        if @abstract_class
+          raise AbstractClassError, "#{self} is an abstract class and cannot be instantiated"
+        else
+          super *args, &block
+        end
       end
-    end
-    
-    meta_def :new, &raise_if_abstract
-    meta_def :allocate, &raise_if_abstract
+      
+      def allocate(*args, &block)
+        if @abstract_class
+          raise AbstractClassError, "#{self} is an abstract class and cannot be instantiated"
+        else
+          super *args, &block
+        end
+      end
+    }
     
     nil
   end
