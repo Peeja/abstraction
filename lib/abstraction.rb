2248c5926e0e1cfb572aa1f91e945dbe82fdc918
pivotal <pivotal@broome.(none)>
2009-12-01 14:53:05 -0500

    Don't clobber @abstract_class

--- lib/abstraction.rb
+++ lib/abstraction.rb
@@ -2,26 +2,26 @@ class AbstractClassError < StandardError; end
 
 class Class
   def abstract
-    @abstract_class = true
-    
+    @abstraction_abstract_class = true
+
     self.extend Module.new {
       def new(*args, &block)
-        if @abstract_class
+        if @abstraction_abstract_class
           raise AbstractClassError, "#{self} is an abstract class and cannot be instantiated"
         else
           super *args, &block
         end
       end
-      
+
       def allocate(*args, &block)
-        if @abstract_class
+        if @abstraction_abstract_class
           raise AbstractClassError, "#{self} is an abstract class and cannot be instantiated"
         else
           super *args, &block
         end
       end
     }
-    
+
     nil
   end
 end
